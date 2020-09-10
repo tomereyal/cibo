578da0a851c0087ea64ba27a883f625f2535f6c0
Tomer Eyal <tomereyal93@gmail.com>
2020-09-10 18:11:53 +0300

    first commit

---
+++ src/components/header/index.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+export interface IPropsHeader {
+title: string ;
+imageUrl?: string; 
+}
+export default function Header(props: IPropsHeader) {
+return  ;
+}
